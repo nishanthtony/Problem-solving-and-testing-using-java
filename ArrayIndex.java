e2cf31f4cc52263c72f4619a49072ab827b39b3e
nishanthtony <vtu24506@veltech.edu.in>
2025-12-31 10:55:49 +0530

    Add ArrayIndex class to print array elements

---
+++ ArrayIndex.java
@@ -0,0 +1,12 @@
+class ArrayIndex{
+    public static void main(String[] args) {
+        
+        int[] arr={10,11,12,13,14};
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+ " " );
+        }
+        
+    }
+}
