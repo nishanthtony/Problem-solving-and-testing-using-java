ec2494c6c43105f300e3f609f56d28cbfe5a0c30
Nishanth G <vtu24506@veltech.edu.in>
2026-01-23 14:33:37 +0530

    Update ishappy.java

--- ishappy.java
+++ ishappy.java
@@ -1,34 +1,23 @@
-class Solution {
-
-    public static int get_next(int n)
-    {
-        int sum=0;
-
-        while(n>0)
-        {
-            int digit=n%10;
-            sum= sum + digit*digit;
-            n=n/10;
-        }
-
-        return sum;
-
-    }
-    public boolean isHappy(int n) {
-
-        int slow=n;
-        int fast = get_next(n);
-
-
-        while(fast!=1 && slow!=fast)
-        {
-            slow=get_next(slow);
-            fast=get_next(get_next(fast));
-            
-        }
-
-        return fast==1;
-        
-        
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	    
+	    int n = 2;
+	    
+	    while(n!=1 && n!=4)
+	    {
+	        int sum=0;
+	        
+	        while(n>0)
+	        {
+	            int digit = n%10;
+	            sum=sum+digit*digit;
+	            n=n/10;
+	        }
+	        
+	        n=sum;
+	    }
+	    
+	    System.out.println(n==1);
+	}
 }
